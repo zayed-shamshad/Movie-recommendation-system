ae1a837c024751834480796d43a4aba67a8cdd6f
Zayed <shamshad.1@iitj.ac.in>
2022-04-26 04:31:23 +0530

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+<div class="entry"><box id='boxx'>lol</box>
+</div>
+</template>
+<script>
+import BOX from './components/BOX.vue'
+export default{
+  name:'App',
+  components:{'box':BOX}
+}
+</script>
+<style lang="scss">
+@import "~@/assets/scss/vendors/bootstrap-vue/index";
+.entry{
+  background-image: url("./assets/movie poster 2.jpg");
+  width:100vw;
+  height:100vh;
+  margin:0;
+  padding:0;
+  z-index: -10;
+}
+*{
+margin: 0;
+padding: 0;
+}
+#boxx{
+  height:50vh;
+  width:60vw;
+  position: absolute;
+  top:20vh;
+  background-color: rgba(0,0,0,.5);
+  color: #fff;
+  border-radius: 10px;
+  left:20vw;
+  border:2px solid white
+}
+</style>
---
+++ src/components/BOX.vue
@@ -0,0 +1,12 @@
+<template>
+ <div id="outside-box">
+     this is a box
+ </div>
+</template>
+<script >
+export default {
+    name:'box'
+}
+</script>
+<style>
+</style>
